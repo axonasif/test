d1050fad38a52c0cb8b559ba1e5bb13bd98b3f70
Aminur Rahman <axonasif@gmail.com>
2023-06-01 18:30:57 +0000

    Up

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Auto start tmux on SSH or xtermjs
+cat >> "$HOME/.bashrc" <<'EOF'
+if test ! -v TMUX && (test -v SSH_CONNECTION || test "$PPID" == "$(pgrep -f '/ide/xterm/bin/node /ide/xterm/index.cjs' | head -n1)"); then {
+  if ! tmux has-session 2>/dev/null; then {
+    tmux new-session -n "editor" -ds "gitpod"
+  } fi
+  exec tmux attach
+} fi
+EOF
+
+curl -L "https://raw.githubusercontent.com/axonasif/gitpod.tmux/main/gitpod.tmux" --output ~/gitpod.tmux
+chmod +x ~/gitpod.tmux
+! grep -q 'gitpod.tmux' ~/.tmux.conf 2>/dev/null && echo "run-shell -b 'exec ~/gitpod.tmux'" >> ~/.tmux.conf
